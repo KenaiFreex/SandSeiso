74670b85b872d69c7d8edc4adaea134e2b7f05bf
KenaiFreex <virtualfreex@gmail.com>
2019-05-02 02:59:00 -0500

    Adding PWM functionality to ManualControl

--- sockets/ManualControl/direction.js
+++ sockets/ManualControl/direction.js
@@ -1,47 +1,125 @@
-var Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+//const Gpio = require('onoff').Gpio; //include onoff to interact with the GPIO
+const Gpio = require('pigpio').Gpio;
 
 
 
-var LED = {
-    "Gpio12": new Gpio(12, 'out'),
-    'Gpio19': new Gpio(19, 'out'),
-    "Gpio26": new Gpio(26, 'out'),
-    "Gpio24": new Gpio(24, 'out'),
-    "Gpio23": new Gpio(23, 'out'),
-    "Gpio17": new Gpio(17, 'out'),
+
+
+const LED = {
+
+    //motor1
+    "R_EN0": new Gpio(13, {
+        mode: Gpio.OUTPUT
+    }),
+    'L_EN0': new Gpio(12, {
+        mode: Gpio.OUTPUT
+    }),
+    "RPWM0": new Gpio(19, {
+        mode: Gpio.OUTPUT
+    }),
+    "LPWM0": new Gpio(26, {
+        mode: Gpio.OUTPUT
+    }),
+    //motor2
+    "R_EN1": new Gpio(24, {
+        mode: Gpio.OUTPUT
+    }),
+    "L_EN1": new Gpio(23, {
+        mode: Gpio.OUTPUT
+    }),
+    "RPWM1": new Gpio(17, {
+        mode: Gpio.OUTPUT
+    }),
+    "LPWM1": new Gpio(18, {
+        mode: Gpio.OUTPUT
+    }),
 
 }
 
 
 
+function ActivarGPIO(direccion,pwmLevel, data) {
 
-function ActivarGPIO(direccion, data) {
+    let datos = data.mensaje;
+    let estado = data.estado;
 
-    datos = data.mensaje;
-    estado = data.estado;
+    let dutyCycle = (direccion == "arriba") ? {
 
-    gpio = (direccion == "arriba") ? LED.Gpio12 :
-        (direccion == "abajo") ? LED.Gpio19 :
-        (direccion == "derecha") ? LED.Gpio26 :
-        (direccion == "izquierda") ? LED.Gpio24 :
-        null;
+            //motor1
+            "R_EN0": 255,
+            'L_EN0': 255,
+            "RPWM0": pwmLevel,
+            "LPWM0": 0,
+            //motor2
+            "R_EN1": 255,
+            "L_EN1": 255,
+            "RPWM1": 0,
+            "LPWM1": pwmLevel,
+        } :
+        (direccion == "abajo") ? {
+            //motor1
+            "R_EN0": 255,
+            'L_EN0': 255,
+            "RPWM0": 0,
+            "LPWM0": pwmLevel,
+            //motor2
+            "R_EN1": 255,
+            "L_EN1": 255,
+            "RPWM1": pwmLevel,
+            "LPWM1": 0,
+        } :
+        (direccion == "derecha") ? {
+            //motor1
+            "R_EN0": 255,
+            'L_EN0': 255,
+            "RPWM0": 255,
+            "LPWM0": 0,
+            //motor2
+            "R_EN1": 255,
+            "L_EN1": 255,
+            "RPWM1": pwmLevel,
+            "LPWM1": 0,
+        } :
+        (direccion == "izquierda") ? {
+            //motor1
+            "R_EN0": 255,
+            'L_EN0': 255,
+            "RPWM0": 0,
+            "LPWM0": pwmLevel,
+            //motor2
+            "R_EN1": 255,
+            "L_EN1": 255,
+            "RPWM1": 0,
+            "LPWM1": 255,
+        } : {
+            //motor1
+            "R_EN0": 0,
+            'L_EN0': 0,
+            "RPWM0": 0,
+            "LPWM0": 0,
+            //motor2
+            "R_EN1": 0,
+            "L_EN1": 0,
+            "RPWM1": 0,
+            "LPWM1": 0,
+        };
 
-    gpioNumber = (direccion == "arriba") ? "LED.Gpio12" :
-        (direccion == "abajo") ? "LED.Gpio19" :
-        (direccion == "derecha") ? "LED.Gpio26" :
-        (direccion == "izquierda") ? "LED.Gpio24" :
-        null;
+     
 
     if (estado == true) {
+        for(let i in LED){
+            LED[i].pwmWrite(dutyCycle[i]);
+            console.log(i +" " + String(dutyCycle[i]));
+        }
 
-        gpio.writeSync(1);
-        console.log(gpioNumber + " encendido");
 
     } else {
-        console.log(gpioNumber + " apagado");
-        gpio.writeSync(0);
-
+        for(let i in LED){
+            LED[i].pwmWrite(0);
+            console.log(i + " 0");
+        }
     }
+
     console.log(datos);
     console.log(estado);
 
